8d75c750f2e2c534f3d263929cf1158e44eff23a
zain <zain.9496@gmail.com>
2020-06-16 20:35:50 +0000

    Project 1

---
+++ .Trash-0/files/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/scripts/myscript.cpp.o"
+  "libproject1.pdb"
+  "libproject1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
